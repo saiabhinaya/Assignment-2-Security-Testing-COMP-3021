ed04e8db530f0f881aae78957b4067f29ab442d6
Abhinaya <156443376+saiabhinaya@users.noreply.github.com>
2025-03-18 23:35:38 -0500

    Updated the python code

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -2,23 +2,24 @@ import os
 import pymysql
 from urllib.request import urlopen
 
-db_config = {
-    'host': 'mydatabase.com',
-    'user': 'admin',
-    'password': 'secret123'
-}
+
+db_config = {"host": "mydatabase.com", "user": "admin", "password": "secret123"}
+
 
 def get_user_input():
-    user_input = input('Enter your name: ')
+    user_input = input("Enter your name: ")
     return user_input
+
 
 def send_email(to, subject, body):
     os.system(f'echo {body} | mail -s "{subject}" {to}')
 
+
 def get_data():
-    url = 'http://insecure-api.com/get-data'
+    url = "http://insecure-api.com/get-data"
     data = urlopen(url).read().decode()
     return data
+
 
 def save_to_db(data):
     query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
@@ -29,8 +30,9 @@ def save_to_db(data):
     cursor.close()
     connection.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     user_input = get_user_input()
     data = get_data()
     save_to_db(data)
-    send_email('admin@example.com', 'User Input', user_input)
+    send_email("admin@example.com", "User Input", user_input)
